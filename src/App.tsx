8e4b07f197f3c5e822213354bb5828d164a0b7fa
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-24 13:20:12 +0300

    App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { Home } from './pages/Home';
+import { Store } from './pages/Store';
+import { About } from './pages/About';
+import { Navbar } from './components/Navbar';
+import { ShoppingCartProvider } from './context/ShoppingCartContext';
+import NotFound from './pages/404';
+
+function App() {
+  return (
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
+  );
+}
+
+export default App;
